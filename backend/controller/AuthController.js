6cd27aedf0c11567be04b4d0d669b93f9131478c
Jyoti Patewar <jyotipatewar2004@gmail.com>
2026-06-30 16:11:34 +0530

    new4

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -22,14 +22,14 @@ dotenv.config();
 
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // MUST be App Password
+    pass: process.env.EMAIL_PASS,
   },
-  secure: true,
 });
-
 
 // ======================
 // SEND OTP
