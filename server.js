155157e818f26463c986b26a2adf1f666d8f9285
Jyoti Patewar <jyotipatewar2004@gmail.com>
2026-06-30 08:08:44 +0530

    Fix Nodemailer SMTP configuration

--- server.js
+++ server.js
@@ -47,47 +47,47 @@ app.listen(process.env.PORT, () => {
 
 
 
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
 
-import { conn } from "./conn/conn.js";
-import RequestRoutes from "./routes/RequestRoutes.js";
-import AuthRoutes from "./routes/AuthRoutes.js";
-import UserRoutes from "./routes/UserRoutes.js";
-import LocationRoutes from "./routes/LocationRoutes.js";
-import DriverRoutes from "./routes/DriverRoutes.js";
-import SupervisorRoutes from "./routes/SupervisorRoutes.js";
+// import { conn } from "./conn/conn.js";
+// import RequestRoutes from "./routes/RequestRoutes.js";
+// import AuthRoutes from "./routes/AuthRoutes.js";
+// import UserRoutes from "./routes/UserRoutes.js";
+// import LocationRoutes from "./routes/LocationRoutes.js";
+// import DriverRoutes from "./routes/DriverRoutes.js";
+// import SupervisorRoutes from "./routes/SupervisorRoutes.js";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
+// const app = express();
 
-// Database Connection
-conn();
+// // Database Connection
+// conn();
 
-// Middlewares
-app.use(express.json());
-app.use(cors());
+// // Middlewares
+// app.use(express.json());
+// app.use(cors());
 
-// Routes
-app.use("/api/auth", AuthRoutes);
-app.use("/api/user", UserRoutes);
-app.use("/api/emergency", RequestRoutes);
-app.use("/api/location", LocationRoutes);
-app.use("/api/driver", DriverRoutes);
-app.use("/api/supervisor", SupervisorRoutes);
+// // Routes
+// app.use("/api/auth", AuthRoutes);
+// app.use("/api/user", UserRoutes);
+// app.use("/api/emergency", RequestRoutes);
+// app.use("/api/location", LocationRoutes);
+// app.use("/api/driver", DriverRoutes);
+// app.use("/api/supervisor", SupervisorRoutes);
 
-// Test Route
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "Backend Running Successfully",
-  });
-});
+// // Test Route
+// app.get("/", (req, res) => {
+//   res.json({
+//     success: true,
+//     message: "Backend Running Successfully",
+//   });
+// });
 
-// Export app instead of app.listen()
-export default app;
+// // Export app instead of app.listen()
+// export default app;
 
 
 
